4926df4a5f051d6c066a1b984b8bf6fef6e5d2d4
MilesWilliams <miles@klarity.co.za>
2016-10-21 13:01:57 +0200

    Added generic page.php

--- wp-content/themes/kommerce/index.php
+++ wp-content/themes/kommerce/index.php
@@ -21,9 +21,9 @@ get_header();
 			if( is_front_page() ){
           get_template_part( 'includes/pages/page', 'home' );
 					}
-				elseif (is_singular() ) {
-
-				}
+			else {
+				get_template_part( 'includes/pages/page' );
+			}
 		endwhile;
 
 	endif;
