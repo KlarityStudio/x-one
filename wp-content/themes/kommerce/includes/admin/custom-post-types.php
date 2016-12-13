6ac8af67b52b29790c09a2207f54623ccf1b03d5
MilesWilliams <miles@klarity.co.za>
2016-12-13 14:05:35 +0200

    Added custom Support Header metabox to Products CPT

--- wp-content/themes/kommerce/includes/admin/custom-post-types.php
+++ wp-content/themes/kommerce/includes/admin/custom-post-types.php
@@ -126,3 +126,66 @@
 	}
 
 	add_action('save_post', 'post_meta_save');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//Adding a Product Support Header
+	/*-----------     Post Type Metaboxes    -----------*/
+
+		function product_custom_metaboxes(){
+			add_meta_box(
+				'postAdd',
+				"Product Title Support",
+				'meta_callback',
+				'product'
+			);
+
+		}
+
+		add_action('add_meta_boxes', 'product_custom_metaboxes');
+
+
+		function meta_callback( $post ){
+
+			wp_nonce_field( basename( __FILE__ ), 'product_post_nonce');
+			$support_stored_meta = get_post_meta( $post->ID ); ?>
+			<div class="meta_row support_title">
+				<div class="meta_th">
+					<label for="support_title" class="meta_label">Enter The Event's start date.</label>
+				</div>
+				<div class="meta_td">
+					<input type="text" name="support_title" id="support_title"value="<?php  if ( ! empty( $support_stored_meta['support_title'] ) ) echo esc_attr( $support_stored_meta['support_title'][0] ); ?>"/>
+				</div>
+			</div>
+			<?php
+		}
+
+		function meta_save( $post_id ){
+			//checks save status
+
+			$is_autosave = wp_is_post_autosave( $post_id );
+			$is_revision = wp_is_post_revision( $post_id );
+			$is_valid_nonce = ( isset( $_POST['product_post_nonce'] ) && wp_verify_nonce( $_POST['product_post_nonce'], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+			//Exit script depending on save status
+
+			if ( $is_autosave || $is_revision || !$is_valid_nonce ){
+				return;
+			}
+			if ( isset( $_POST[ 'support_title' ] ) ) {
+				update_post_meta( $post_id, 'support_title', sanitize_text_field($_POST[ 'support_title' ] ) );
+			}
+		}
+
+		add_action('save_post', 'meta_save');
