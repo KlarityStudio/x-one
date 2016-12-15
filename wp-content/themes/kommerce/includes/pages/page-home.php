15253b65b1419bae9465565b8b51413c5cc3b010
Andy <andy@klarity.co.za>
2016-12-15 12:59:19 +0200

    more home updates

--- wp-content/themes/kommerce/includes/pages/page-home.php
+++ wp-content/themes/kommerce/includes/pages/page-home.php
@@ -10,7 +10,7 @@
         <iframe src="https://www.youtube.com/embed/1xm7ZsfV6_s" width="690" height="358" frameborder="0" allowfullscreen></iframe>
         <iframe src="https://www.youtube.com/embed/YSVgH6DVUBA" width="690" height="358" frameborder="0" allowfullscreen></iframe>
     </section>
-   <div class="button-wrapper"><a href="<?php echo site_url('/product-category/screen-protectors'); ?>">View Screen Protectors</a></div>
+   <!-- <div class="button-wrapper"><a href="<?php echo site_url('/product-category/screen-protectors'); ?>">View Screen Protectors</a></div> -->
 </article>
 <article class="category-section category-cases">
     <img class="category-header-image" src="/wp-content/themes/kommerce/_build/images/snakeskin-cover.png" alt="Mobile Cases">
@@ -19,11 +19,11 @@
     <section class="product-list">
         <?php echo do_shortcode('[infinity id="751"]'); ?>
     </section>
-    <div class="button-wrapper"><a href="<?php echo site_url('/product-category/phone-covers'); ?>">View Phone Covers</a></div>
+    <!-- <div class="button-wrapper"><a href="<?php echo site_url('/product-category/phone-covers'); ?>">View Phone Covers</a></div> -->
 </article>
 <article class="category-section category-cables">
     <img class="category-header-image" src="/wp-content/themes/kommerce/_build/images/apple-cable.png" alt="Apple Cable">
-    <h1>Braided USB Cables</h1>
+    <h1>Cables &amp; Cases</h1>
     <?php echo categoryDescriptions( 'usb-cables'); ?>
     <section class="product-list">
         <?php echo do_shortcode('[infinity id="763"]'); ?>
@@ -31,5 +31,5 @@
     <section class="video-wrapper video-cables">
         <iframe src="https://www.youtube.com/embed/YSVgH6DVUBA" width="690" height="358" frameborder="0" allowfullscreen></iframe>
     </section>
-   <div class="button-wrapper"><a href="<?php echo site_url('/product-category/usb-cables'); ?>">View Usb Cables</a></div>
+   <!-- <div class="button-wrapper"><a href="<?php echo site_url('/product-category/usb-cables'); ?>">View Usb Cables</a></div> -->
 </article>
