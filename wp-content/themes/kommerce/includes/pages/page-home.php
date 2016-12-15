1ed03aae40b8e1204d7e8f7b2e6f3c31e31e94eb
Andy <andy@klarity.co.za>
2016-12-15 12:59:02 +0200

    home page updates

--- wp-content/themes/kommerce/includes/pages/page-home.php
+++ wp-content/themes/kommerce/includes/pages/page-home.php
@@ -1,9 +1,9 @@
+
 <article class="category-section category-screen-protectors">
     <h1 class="git-test">Screen Protectors</h1>
     <?php echo categoryDescriptions( 'screen-protectors'); ?>
     <section class="product-list">
-            <!-- <?php echo do_shortcode('[product_category category="screen-protectors"]'); ?> -->
-            <?php  echo productSlider('screen-protectors'); ?>
+            <?php echo do_shortcode('[infinity id="693"]'); ?>
     </section>
 
     <section class="video-wrapper video-screen-protectors">
@@ -17,8 +17,7 @@
     <h1>Mobile Cases</h1>
     <?php echo categoryDescriptions( 'phone-covers'); ?>
     <section class="product-list">
-        <!-- <?php echo do_shortcode('[product_category category="phone-covers"]'); ?> -->
-        <?php  echo productSlider('phone-covers'); ?>
+        <?php echo do_shortcode('[infinity id="751"]'); ?>
     </section>
     <div class="button-wrapper"><a href="<?php echo site_url('/product-category/phone-covers'); ?>">View Phone Covers</a></div>
 </article>
@@ -27,8 +26,7 @@
     <h1>Braided USB Cables</h1>
     <?php echo categoryDescriptions( 'usb-cables'); ?>
     <section class="product-list">
-        <!-- <?php echo do_shortcode('[product_category category="usb-cables"]'); ?> -->
-        <?php  echo productSlider('usb-cables'); ?>
+        <?php echo do_shortcode('[infinity id="763"]'); ?>
     </section>
     <section class="video-wrapper video-cables">
         <iframe src="https://www.youtube.com/embed/YSVgH6DVUBA" width="690" height="358" frameborder="0" allowfullscreen></iframe>
