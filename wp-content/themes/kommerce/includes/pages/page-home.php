bc5bafec5c4f50e3719f9213c4c7e079e5d6fc06
MilesWilliams <miles@klarity.co.za>
2016-12-19 12:36:16 +0200

    Single product page media queries

--- wp-content/themes/kommerce/includes/pages/page-home.php
+++ wp-content/themes/kommerce/includes/pages/page-home.php
@@ -8,13 +8,13 @@
     </section>
 
     <section class="video-wrapper video-screen-protectors">
-        <iframe src="https://www.youtube.com/embed/1xm7ZsfV6_s" width="690" height="358" frameborder="0" allowfullscreen></iframe>
-        <iframe src="https://www.youtube.com/embed/YSVgH6DVUBA" width="690" height="358" frameborder="0" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/8XBGLImhq9w" width="690" height="358" frameborder="0" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/Js0W_zR5CPQ" width="690" height="358" frameborder="0" allowfullscreen></iframe>
     </section>
    <!-- <div class="button-wrapper"><a href="<?php echo site_url('/product-category/screen-protectors'); ?>">View Screen Protectors</a></div> -->
 </article>
 <article class="category-section category-cases">
-    <img class="category-header-image" src="/wp-content/themes/kommerce/_build/images/snakeskin-cover.png" alt="Mobile Cases">
+    <img class="category-header-image" src="/wp-content/themes/kommerce/_build/images/leather.png" alt="Mobile Cases">
     <h1>Mobile Cases</h1>
     <?php echo categoryDescriptions( 'phone-covers'); ?>
     <section class="product-list">
@@ -28,7 +28,7 @@
     <section class="product-list">
     </section>
     <section class="video-wrapper video-cables">
-        <iframe src="https://www.youtube.com/embed/YSVgH6DVUBA" width="690" height="358" frameborder="0" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/VBCi851OlVw" width="690" height="358" frameborder="0" allowfullscreen></iframe>
     </section>
    <!-- <div class="button-wrapper"><a href="<?php echo site_url('/product-category/usb-cables'); ?>">View Usb Cables</a></div> -->
 </article>
