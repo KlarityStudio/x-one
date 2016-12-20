7c4e4d81c46563ae0bf000a9d42995a4ae5706b3
MilesWilliams <miles@klarity.co.za>
2016-12-20 12:57:58 +0200

    Changed look of quantity box on cart page, added hotjar script

--- wp-content/themes/kommerce/header.php
+++ wp-content/themes/kommerce/header.php
@@ -23,6 +23,17 @@
 
         </script>
         <!--  Google Analytics end -->
+        <!-- Hotjar Tracking Code for http://x-one.co.za/ -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:372246,hjsv:5};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
         <script type="application/ld+json">
 
             { "@context": "http://schema.org",
